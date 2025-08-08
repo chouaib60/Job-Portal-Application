9bab411b66052a43bf61c71279148a9ea41d45ce
chouaib <khomallic@gmail.com>
2025-08-08 23:44:05 +0100

    daba khdem

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
+import Navbar from "../components/Navbar";
+
 const Home = () => {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Accueil</h1>
-      <p>Bienvenue sur notre portail d'emploi</p>
+    <div>
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

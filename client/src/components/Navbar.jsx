ff2166bb851bffaae4d38c1d1e520c96d36334e9
chouaib <khomallic@gmail.com>
2025-08-09 02:40:16 +0100

    tania

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
 import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div className='shadow py-4 px-6 flex justify-between items-center'>
+      <div className='container px-4 mx-auto flex justify-between items-center'>
         <img src={assets.logo} alt="" />
+        <div className='flex gap-4 max-sm:text-xs'> 
+            <button className='text-gray-600'>Recruiter Login</button>
+            <button className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
+        </div>
       </div>
     </div>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,26 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppProvider } from './context/AppContext.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Récupération de la clé publique depuis .env
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
+      <ClerkProvider
+        publishableKey={PUBLISHABLE_KEY}
+        navigate={(to) => window.history.pushState(null, '', to)}
+      >
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
 )

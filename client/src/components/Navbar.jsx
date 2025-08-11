56e2771d7862af352f295834585c234e14e0f1ea
chouaib <khomallic@gmail.com>
2025-08-12 00:41:30 +0100

    tanin

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import { assets } from '../assets/assets'
-
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+//userButton est un composant pret à l'emploi qui affiche la photo et les options de compte de l'utilisateur connecté
+// useUser est un hook qui permet de récupérer les informations de l'utilisateur connecté
 const Navbar = () => {
+
+  const { openSignIn } = useClerk() // useClerk est un hook qui permet d'accéder aux fonctions globales de Clerk (comme fonction qui permet d'ouvrir la fenetre de connexion"opensignin , déconexion etc)
+  const { user } = useUser() // useUser est un hook qui permet de récupérer les informations de l'utilisateur connecté 
   return (
-    <div className='shadow py-4 px-6 flex justify-between items-center'>
+    <div className='shadow py-4 px-6 flex justify-between items-center'> {/*classname permet d'appliquer des classes CSS à un élément JSX  */}
       <div className='container px-4 mx-auto flex justify-between items-center'>
         <img src={assets.logo} alt="" />
-        <div className='flex gap-4 max-sm:text-xs'> 
-            <button className='text-gray-600'>Recruiter Login</button>
-            <button className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
-        </div>
+        {
+          user
+            ? <div></div>
+            : <div className='flex gap-4 max-sm:text-xs'>
+                <button className='text-gray-600'>Recruiter Login</button>
+                <button onClick={e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button> {/*opensignin est une fonction de clerk qui permet d'ouvrir la fenetre de connecion */}
+              </div>
+        }
       </div>
     </div>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,10 +16,13 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ClerkProvider
+      <ClerkProvider // clerk provider c'est contexte qui va rendre disponibles les infos et fonctions clerk dans tout ton projet
+      // PUBLISHABLE_KEY est la clé public de mon projet pour identifier mon application chez clerk
         publishableKey={PUBLISHABLE_KEY}
         navigate={(to) => window.history.pushState(null, '', to)}
-      >
+        // navigate c'est une fonction clerk qui permet de gérer la navigation dans l'application , c'est pour rediriger l'utilisateur (par exemple : quand il clique sur login ou logout il est redirigé vers une autre page)
+        // on utilise window.history.pushState pour mettre à jour l'URL sans recharger la page
+      > 
         <AppProvider>
           <App />
         </AppProvider>
